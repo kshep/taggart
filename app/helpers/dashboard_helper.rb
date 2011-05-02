0a05e77729131d743b021483977a292eb295d6b3
Ken Sheppardson <github@kshep.net>
2011-05-02 13:44:28 -0700

    Further implementation of Dynatree and Flot

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,23 @@
 module DashboardHelper
+  def list
+    data = Event.all.last['data']
+    @result = "<ul id='treeData'>"
+    traverse(data,[],"  ")
+    @result <<  "</ul>"
+    @result
+  end
+
+  def traverse(e, path, pre)
+    e.each do |k,v|
+      if v.respond_to?('each')
+        @result << "#{pre}<li data='hideCheckbox: true'>#{k}"
+        @result << "#{pre}  <ul>"
+        traverse(v, path+[k], pre+"    ")
+        @result << "#{pre}  </ul>"
+        @result << "#{pre}</li>"
+      else
+        @result << "#{pre}<li id='#{(path+[k]).compact.join('|')}'>#{k}</li>" if v.is_a?(Numeric)
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Taggart::Application.routes.draw do
   root :to => "dashboard#view"
 
   resources :events
+  resources :data
 
 end
