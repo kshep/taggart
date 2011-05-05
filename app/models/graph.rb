253e95aab9493b445e0067fba5c306e97d1d8f8c
Ken Sheppardson <github@kshep.net>
2011-05-05 10:15:33 -0700

    Annotate models

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+# Schema version: 20110505161202
+#
+# Table name: graphs
+#
+#  id         :integer         not null, primary key
+#  user_id    :integer
+#  caption    :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Graph < ActiveRecord::Base
   belongs_to :user
 end
