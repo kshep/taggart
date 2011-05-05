a4d7c19eded022ace6ebb8bd4e36723aa8101436
Ken Sheppardson <github@kshep.net>
2011-05-05 12:55:44 -0700

    Cleanup JQuery, datepicker, and info block

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,45 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+function clear_tree() {
+
+  $("#tree").dynatree("getRoot").visit(function(node){
+        node.select(false);
+  });
+
+}
+
+function get_selected() {
+  var root = $('#tree').dynatree("getRoot");
+  var keys = $.map(root.tree.getSelectedNodes(), function(node){
+    return node.data.key;
+  });
+  return keys;
+}
+
 function draw_graphs() {
 
-    draw_graph('#graph', 'db_stats:test:avgObjSize');
+
+  $('.flash').remove();
+
+  keys = get_selected();
+
+  $("#info_keys").text(keys.join(", "));
+
+  $('#graph_wrapper').empty();
+  for (var i in keys) {
+    var div = $('<div>');
+    div.append($('<div>')
+      .attr('class','graph_title')
+      .text(keys[i]));
+    div.append($('<div>')
+      .attr('id','graph'+i)
+      .attr('class','graph')
+    );
+    $('#graph_wrapper').append(div);
+    draw_graph('#graph'+i, keys[i]);
+  }
+
 }
 
 function draw_graph(div, key) {
@@ -18,9 +54,17 @@ function draw_graph(div, key) {
          data = json;
        }
      });
-    
+
+ 
+  
+  var min_date = new Date($('#start_datetime').val());
+  var max_date = new Date($('#end_datetime').val());
+ 
+  var graph_min = min_date.getTime();
+  var graph_max = max_date.getTime();
+ 
   var options = {
-      xaxis: { mode: "time", timeformat: "%m/%d %h:%M" },
+      xaxis: { mode: "time", timeformat: "%m/%d %h:%M", min: graph_min, max: graph_max },
       legend: { position: "se" }
   };
 
@@ -32,27 +76,10 @@ function draw_tree() {
   
   $.ui.dynatree.nodedatadefaults["icon"] = false; // Turn off icons
 
-  $("#tree").dynatree({
-    checkbox: true,
+  $("#tree").dynatree({ checkbox: true,
     onSelect: function(select, node) {
-      var selKeys = $.map(node.tree.getSelectedNodes(), function(node){
-        return node.data.key;
-      });
-      $('.flash').remove();
-      $("#info").text(selKeys.join(", "));
-      $('#graph_wrapper').empty();
-      for (var i in selKeys) {
-        var div = $('<div>');
-        div.append($('<div>')
-          .attr('class','graph_title'))
-          .text(selKeys[i]);
-        div.append($('<div>')
-          .attr('id','graph'+i)
-          .attr('class','graph')
-        );
-        $('#graph_wrapper').append(div);
-        draw_graph('#graph'+i, selKeys[i]);
-      }
+      keys = get_selected();
+      $("#info_keys").text(keys.join(", "));
     }
   });
   
