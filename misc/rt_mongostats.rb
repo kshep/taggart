1f7b37d5fb12ca3d11a3aabddf4b2515a476cb9f
Your Name <ken@kshep.net>
2011-05-09 14:25:48 -0700

    Add script to continuously submit db stats

---
+++ misc/rt_mongostats.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'net/http'
+require 'mongo'
+require 'json'
+require 'uri'
+
+timer = ARGV[0].to_i || 10 # How long to wait between checks in sec
+
+conn = Mongo::Connection.new
+
+print "Collecting stats"
+loop {
+  db_names = conn.database_names
+  
+  server_status = conn[db_names[0]].command('serverStatus' => 1)
+  
+  db_stats = {}
+  
+  db_names.each do |db_name|
+    db_stats[db_name] = conn[db_name].stats()
+  end
+  
+  report = { 'time'          => Time.now,
+             'server_status' => server_status,
+             'db_stats'      => db_stats
+           }.to_json
+  
+  uri = URI.parse("http://taggart.dev/events")
+  http = Net::HTTP.new(uri.host, uri.port)
+  req  = Net::HTTP::Post.new(uri.request_uri)
+  req['Content-Type'] = 'application/json'
+  req.body = report
+  
+  res = http.request(req)
+
+  print "."
+  sleep(timer)
+
+}
